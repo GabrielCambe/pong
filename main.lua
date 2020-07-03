1bb3511ff5ef893896600bd80404baf13fbb2c47
GabrielCambe <almeidasalesgabriel@gmail.com>
2020-07-03 07:03:05 -0300

    pong-2

--- main.lua
+++ main.lua
@@ -15,6 +15,11 @@ function love.load()
         -- })
 
         love.graphics.setDefaultFilter('nearest', 'nearest')
+        
+        -- get and set a font object to use in the game
+        smallfont = love.graphics.newFont('font.ttf', 8)
+        love.graphics.setFont(smallfont)
+
         push:setupScreen(
             VIRTUAL_WIDTH, VIRTUAL_HEIGHT,
             WINDOW_WIDTH, WINDOW_HEIGHT,{
@@ -41,12 +46,21 @@ function love.draw()
     --     'center'
     -- )
 
+    -- wash the screen with the RGBA color passed as an argument
+    love.graphics.clear((40/255), (45/255), (52/255), (255/255))
+
     love.graphics.printf(
         'Hello World!',
-        0, ((VIRTUAL_HEIGHT/2)-6),
+        0, 20,
         VIRTUAL_WIDTH,
         'center'
     )
+
+    -- the paddles and the ball are rectangles that we draw now and then
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', (VIRTUAL_WIDTH - 10), (VIRTUAL_HEIGHT - 50), 5, 20)
+    -- the ball
+    love.graphics.rectangle('fill', ((VIRTUAL_WIDTH/2)-2), ((VIRTUAL_HEIGHT/2)-2), 4, 4) 
 
     push:finish()
 end
